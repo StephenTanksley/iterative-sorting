31c44659121e4752f189fbe531c0d2a0601bb475
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-07-08 19:58:40 -0500

    Got linear search working.

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -8,9 +8,14 @@ def selection_sort(arr):
         # (hint, can do in 3 loc)
         # Your code here
 
+        # this is our "imaginary line" for sorting.
+        for j in range(i+1, len(arr)):
+            if arr[smallest_index] > arr[j]:
+                smallest_index = j
 
         # TO-DO: swap
         # Your code here
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
 
     return arr
 
@@ -19,8 +24,8 @@ def selection_sort(arr):
 def bubble_sort(arr):
     # Your code here
 
-
     return arr
+
 
 '''
 STRETCH: implement the Counting Sort function below
@@ -39,8 +44,9 @@ buckets.
 
 What is the time and space complexity of the counting sort algorithm?
 '''
+
+
 def counting_sort(arr, maximum=None):
     # Your code here
-
 
     return arr
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,16 @@
 def linear_search(arr, target):
     # Your code here
-
-
-    return -1   # not found
+    for item in arr:
+        if item == target:
+            return arr.index(item)
+    return -1
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
+    sorted = arr.sort()
+    print(sorted)
     # Your code here
-
 
     return -1  # not found
